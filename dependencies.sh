1605dee78f5d06cdb7c02053a5bdc5d65d851bf3
Eric Platon <zaraki@gmx.com>
2019-03-06 15:32:51 +0000

    Library dependency file, and Python requirements
    List as needed to run on a Raspberry Pi. The list surely differs on different distros.

---
+++ dependencies.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y \
+	libhdf5-dev \
+	libatlas3-base \
+	libharfbuzz0b \
+	libwebp6 \
+	libtiff5 \
+	libjasper1 \
+	openexr \
+	libgstreamer-plugins-base1.0-0 \
+	libavcodec57 \
+	libavformat57 \
+	libswscale4 \
+	libqtgui4 \
+	libqt4-test
