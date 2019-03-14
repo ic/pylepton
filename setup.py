84c5c8561362ef21f55f36c2eb4f670ee1630e69
Eric Platon <zaraki@gmx.com>
2019-03-14 18:00:41 +0900

    Modernize setup script.
    Move to setuptools.

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from pkgutil import walk_packages
-
-import pylepton
-
-def find_packages(path=".", prefix=""):
-    yield prefix
-    prefix = prefix + "."
-    for _, name, ispkg in walk_packages(path, prefix):
-        if ispkg:
-            yield name
+from setuptools import find_packages, setup
 
 setup(name='pylepton',
       version='0.1.2',
@@ -18,7 +8,7 @@ setup(name='pylepton',
       author='Kurt Kiefer',
       author_email='kurt@kieferware.com',
       url='https://github.com/kekiefer/pylepton',
-      packages = list(find_packages(pylepton.__path__, pylepton.__name__)),
+      packages = find_packages('.'),
       scripts = ["pylepton_capture", "pylepton_overlay"],
-      install_depends = ['numpy', 'cv2'],
+      install_requires = ['numpy', 'cv2'],
      )
